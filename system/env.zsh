9445536880a7c80ebdc056f91d9e6e764a819131
Joel Gillman <joeltgillman@gmail.com>
2012-04-10 22:02:11 -0400

    Added some exports and the ... alias

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,6 @@
+# Make vim the default editor
 export EDITOR='vim'
+# Don’t clear the screen after quitting a manual page
+export MANPAGER="less -X"
+# Highlight section titles in manual pages
+export LESS_TERMCAP_md="$ORANGE"
