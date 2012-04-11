36be21f402b301e03c88098d7d9a8b7cbb10e6ed
Joel Gillman <joeltgillman@gmail.com>
2012-04-11 10:33:53 -0400

    Make sure the system is using UTF8

--- system/env.zsh
+++ system/env.zsh
@@ -1,3 +1,8 @@
+# Make sure we're using UTF8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
 # Make vim the default editor
 export EDITOR='vim'
 # Don’t clear the screen after quitting a manual page
