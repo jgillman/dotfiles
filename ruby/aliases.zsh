8dee46842bf9c28bf497574517ae46f2cbcd5c06
Joel Gillman <joeltgillman@gmail.com>
2014-03-24 16:37:53 -0700

    convenience aliases for starting rails consoles

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -3,6 +3,7 @@ alias brs='b rspec'
 alias migrateall='b rake db:migrate db:test:prepare'
 alias pbm='git pull; bundle; migrateall;'
 
+alias railsc='GOLDBELY_ADMIN=true GOLDBELY_PUBLIC=true RACK_ENV=development rails console'
 alias prodc='heroku run console --app goldbely'
 alias stagc='heroku run console --app goldbely-staging'
 
