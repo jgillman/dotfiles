c1b51aa90ab8ddf257a8c006442e5964f00d16e4
Joel Gillman <joeltgillman@gmail.com>
2014-08-12 00:25:56 +0000

    Remove unused todo stuff from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -62,25 +62,6 @@ rb_prompt(){
   fi
 }
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
 directory_name(){
   echo "%{$fg[cyan]%}%1/%\/%{$reset_color%}"
 }
