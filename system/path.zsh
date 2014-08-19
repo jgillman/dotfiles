bf9b6c42301a5ead2628c1af832b88054c66b2cc
Joel Gillman <joeltgillman@gmail.com>
2014-08-19 23:59:21 +0000

    Cleanup rbenv config and related PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$HOME/bin:./bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="$HOME/bin:./bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
