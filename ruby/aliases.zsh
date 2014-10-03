26a099528d279d831676c1338bf4efae238fc4c0
Joel Gillman <joeltgillman@gmail.com>
2014-10-02 17:40:54 -0700

    Update ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,6 +1,4 @@
-alias b='bundle exec'
-alias brs='b rspec'
-alias migrateall='b rake db:migrate db:test:prepare'
+alias migrateall='rake db:migrate db:test:prepare'
 alias pbm='git pull; bundle; migrateall;'
 
 alias railsc='GOLDBELY_ADMIN=true GOLDBELY_PUBLIC=true RACK_ENV=development rails console'
