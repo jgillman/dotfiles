f6719cf24795c5ccde0f4fe2b15561c358921263
Joel Gillman <joeltgillman@gmail.com>
2016-09-19 19:38:02 +0000

    Update gpg config

--- gpg/config.zsh
+++ gpg/config.zsh
@@ -1,19 +1,55 @@
-export GPGKEY=13104968
+#
+# Provides for an easier use of GPG by setting up gpg-agent.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
 
-# Use gpg-agent if it's available
-if hash gpg-agent 2>/dev/null; then
+# Return if requirements are not found.
+if (( ! $+commands[gpg-agent] )); then
+  return 1
+fi
 
-  function is_gpg_agent_running() {
-    kill -0 $(cut -d: -f 2 ${HOME}/.gpg-agent-info) 2>/dev/null
-  }
+# Set the default paths to gpg-agent files.
+_gpg_agent_conf="${GNUPGHOME:-$HOME/.gnupg}/gpg-agent.conf"
+_gpg_agent_env="${TMPDIR:-/tmp}/gpg-agent.env"
 
-  if test -f ${HOME}/.gpg-agent-info && is_gpg_agent_running; then
-    GPG_AGENT_INFO=$(cat "${HOME}/.gpg-agent-info" | cut -c 16-)
-  else
-    # Start a gpg-agent session
-    eval $(gpg-agent --daemon --no-grab --write-env-file ${HOME}/.gpg-agent-info)
+# Start gpg-agent if not started.
+if [[ -z "$GPG_AGENT_INFO" && ! -S "${GNUPGHOME:-$HOME/.gnupg}/S.gpg-agent" ]]; then
+  # Export environment variables.
+  source "$_gpg_agent_env" 2> /dev/null
+
+  # Start gpg-agent if not started.
+  if ! ps -U "$LOGNAME" -o pid,ucomm | grep -q -- "${${${(s.:.)GPG_AGENT_INFO}[2]}:--1} gpg-agent"; then
+    eval "$(gpg-agent --daemon | tee "$_gpg_agent_env")"
   fi
+fi
 
-  export GPG_TTY=$(tty)
-  export GPG_AGENT_INFO
+# Inform gpg-agent of the current TTY for user prompts.
+export GPG_TTY="$(tty)"
+
+# Integrate with the SSH module.
+if grep 'enable-ssh-support' "$_gpg_agent_conf" &> /dev/null; then
+  # Load required functions.
+  autoload -Uz add-zsh-hook
+
+  # Override the ssh-agent environment file default path.
+  _ssh_agent_env="$_gpg_agent_env"
+
+  # Load the SSH module for additional processing.
+  pmodload 'ssh'
+
+  # Updates the GPG-Agent TTY before every command since SSH does not set it.
+  function _gpg-agent-update-tty {
+    gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null
+  }
+  add-zsh-hook preexec _gpg-agent-update-tty
+fi
+
+# Clean up.
+unset _gpg_agent_{conf,env}
+
+# Disable GUI prompts inside SSH.
+if [[ -n "$SSH_CONNECTION" ]]; then
+  export PINENTRY_USER_DATA='USE_CURSES=1'
 fi
