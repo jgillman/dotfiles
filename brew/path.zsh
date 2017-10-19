34d20d932a80603b3801cb6fd478b43e2dd7b593
Joel Gillman <joeltgillman@gmail.com>
2017-10-19 15:06:05 -0400

    Fix path issues

--- brew/path.zsh
+++ brew/path.zsh
@@ -1,3 +1,3 @@
-_prepend_to_path "/usr/local/sbin"
 _prepend_to_path "/usr/local/bin"
+_prepend_to_path "/usr/local/sbin"
 export MANPATH="/usr/local/man:$MANPATH"
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
 _prepend_to_path "${ZSH}/bin"
-_prepend_to_path "./bin"
 _prepend_to_path "${HOME}/bin"
