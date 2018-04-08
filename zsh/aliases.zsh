2d42903c63d048210d617e77c8daa79efa2ea573
Trevor Stow <trevorstow@gmail.com>
2018-04-08 09:56:07 +0000

    clean up unused aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,15 +16,6 @@ alias flush="dscacheutil -flushcache"
 # Start a webserver in the current dir, stolen from @stevenmbone
 alias servethis="open http://0.0.0.0:8000 && python -m SimpleHTTPServer"
 
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop"defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop"defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-
-# Stuff I never really use but cannot delete because of http://xkcd.com/530/
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 10'"
-alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
-
 function backup() { cp ${1}{,.bak}; }
 function fname() { find . -iname "*$@*"; }
 function psgrep() { ps axuf | grep -v grep | grep "$@" -i; }
