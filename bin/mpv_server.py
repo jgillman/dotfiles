a0f527f845a6581c294842eb8d66535b932b7957
Joel Gillman <joeltgillman@gmail.com>
2019-01-09 13:45:23 -0500

    Add mpv_server.py

---
+++ bin/mpv_server.py
@@ -0,0 +1,29 @@
+import os
+import subprocess, urlparse
+import SimpleHTTPServer, SocketServer
+PORT = 4040
+
+class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        parsedParams = urlparse.urlparse(self.path)
+        parsed_query = urlparse.parse_qs(parsedParams.query)
+        url = parsed_query['i'][0]
+        self.send_response(204)
+
+        os.system('pkill mpv') #only one
+
+        subprocess.Popen(
+            [
+                'mpv',
+                '--mute=yes', # always start muted
+                '--loop=inf', # loop by default
+                url
+            ],
+            stdout=subprocess.PIPE
+        )
+
+Handler = MyHandler
+httpd = SocketServer.TCPServer(('', PORT), Handler)
+print "serving at port ", PORT
+httpd.serve_forever()
