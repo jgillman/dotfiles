55febd00476ee4d5260ac855a0bb55a26f4c0f7c
Joel Gillman <joeltgillman@gmail.com>
2019-01-09 13:44:40 -0500

    Dump of a bunch of misc changes

--- node/path.zsh
+++ node/path.zsh
@@ -2,3 +2,7 @@
 _prepend_to_path "${HOME}/node_modules/.bin"
 _prepend_to_path "/usr/local/lib/node_modules"
 _append_to_path "/usr/local/share/npm/bin"
+
+# Add path for yarn
+_prepend_to_path "${HOME}/.yarn/bin"
+_prepend_to_path "${HOME}/.config/yarn/global/node_modules/.bin"
