28fac2ff50c340abde2a1478025d55cee4affd96
Joel Gillman <joeltgillman@gmail.com>
2019-04-01 13:28:34 -0400

    Update 'notes' alias to mount encrypted drive

--- notes/aliases.zsh
+++ notes/aliases.zsh
@@ -1,1 +1,8 @@
-alias notes='terminal_velocity'
+function notes() {
+  if ! mount | grep "${HOME}/dropbox-crypt" > /dev/null; then
+    ${HOME}/Dropbox/uncrypt.sh && \
+      terminal_velocity
+  else
+    terminal_velocity
+  fi
+}
