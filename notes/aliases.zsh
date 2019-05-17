b3f49a64837f76ac9bc1bd37e933b416bdd96a23
Joel Gillman <joel@joelgillman.com>
2019-05-17 12:35:12 -0400

    Make decrypting notes more smarterer

--- notes/aliases.zsh
+++ notes/aliases.zsh
@@ -1,8 +1,10 @@
-function notes() {
+function mount_crypt_if_needed() {
   if ! mount | grep "${HOME}/dropbox-crypt" > /dev/null; then
-    ${HOME}/Dropbox/uncrypt.sh && \
-      terminal_velocity
+    ${HOME}/Dropbox/uncrypt.sh
   else
-    terminal_velocity
+    return 0
   fi
 }
+
+alias notes='mount_crypt_if_needed && terminal_velocity'
+alias scratch='mount_crypt_if_needed && vim ~/dropbox-crypt/notes/goldbelly/scratch.md'
