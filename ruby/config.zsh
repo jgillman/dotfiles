c2b574642f58349354e0d6ee1a48cffea21f0ddd
Joel Gillman <joel@joelgillman.com>
2025-06-05 16:05:30 -0500

    Minor rbenv update

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,3 +1,8 @@
 if _has rbenv; then
+  # rbenv
+  export RBENV_ROOT=/opt/homebrew/opt/rbenv
+  export PATH=$RBENV_ROOT/bin:$PATH
   eval "$(rbenv init -)"
 fi
+
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
