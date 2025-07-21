19bc4c7abbb6c879f65e3290c64f0b688990e9b6
Trevor Stow <trevorstow@gmail.com>
2025-07-21 14:28:18 -0700

    clean up zsh aliases vs. git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,11 +3,6 @@ alias reload!='. ~/.zshrc'
 alias bu='bundle '
 alias brs='bin/rspec -f documentation'
 alias brsx='brs $(git status | grep "_spec\.rb" | grep "modified:" | cut -b 14- )'
-alias gdn='git diff --name-only'
-alias gdnm='git diff --name-only master'
-alias grs='git restore --staged'
-alias grm="git reset `git log --oneline master | head -n 1 | awk -F' ' '{print $1}'`"
-alias grbm="git rebase master"
 alias hcons='bin/production run rails c' #  -e DISABLE_DATADOG_AGENT=true --app goldbely'
 alias la='ls -alG'
 alias tmuxa='tmux attach || tmux new'
