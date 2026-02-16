28548553b226cd289c62bc89c0f598ced052642f
Joel Gillman <joel@joelgillman.com>
2026-02-16 16:13:16 -0600

    Clean up colorscheme

--- nvim/config-nvim/lua/plugins/colorscheme.lua
+++ nvim/config-nvim/lua/plugins/colorscheme.lua
@@ -3,25 +3,11 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-    },
   },
   {
     'dracula/vim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-    },
     config = function()
       -- load the colorscheme here
       vim.cmd.colorscheme 'dracula'
