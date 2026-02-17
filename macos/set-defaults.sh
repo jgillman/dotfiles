80ec454b4b18925e2851aa63324b5c7fc54aa4ca
Joel Gillman <joel@joelgillman.com>
2026-02-17 11:58:15 -0600

    Update macos defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -26,10 +26,6 @@ sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.serve
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
-# Reveal IP address, hostname, OS version, etc. when clicking the clock
-# in the login window
-sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-
 
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
@@ -39,9 +35,7 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 ###############################################################################
 
 # Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.AppleMultitouchTrackpad -bool true
 
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
